7e4dc6f427bca5fb84ff9d637b4aeb5bd925664c
mi11ione <i@mi11ion.io>
2024-12-13 12:24:57 +0300

    cool

--- task5/ContentView.swift
+++ task5/ContentView.swift
@@ -10,29 +10,25 @@ import SwiftUI
 struct ContentView: View {
     @State private var position = CGPoint(x: 200, y: 120)
 
+    private let rectColors = [Color.black, .white, .black, .white]
+    private let backColors = [Color.white, .pink, .yellow, .black]
+
     var body: some View {
         ZStack {
-            VStack(spacing: 0) {
-                Color.black
-                Color.white
-                Color.black
-                Color.white
-            }
+            VStack(spacing: 0) { ForEach(rectColors, id: \.self) { $0 } }
 
-            VStack(spacing: 0) {
-                Color.white
-                Color.pink
-                Color.yellow
-                Color.black
-            }
-            .overlay {
-                RoundedRectangle(cornerRadius: 20)
-                    .blendMode(.destinationOut)
-                    .frame(width: 100, height: 100)
-                    .position(x: position.x, y: position.y)
-            }
-            .compositingGroup()
-            .gesture(DragGesture().onChanged { value in position = value.location })
+            VStack(spacing: 0) { ForEach(backColors, id: \.self) { $0 } }
+                .overlay {
+                    RoundedRectangle(cornerRadius: 20)
+                        .blendMode(.destinationOut)
+                        .frame(width: 100, height: 100)
+                        .position(position)
+                }
+                .compositingGroup()
+                .gesture(
+                    DragGesture()
+                        .onChanged { position = $0.location }
+                )
         }
         .ignoresSafeArea()
     }
